b5f9c9652f6590f820f1be3ec9656d0065f69777
gouf <innocent.zero@gmail.com>
2019-01-05 22:30:53 +0900

    キーボード入力で Player を転がす機能を追加

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,5 +2,16 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private void FixedUpdate()
+    {
+        // get user input into x and z
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
+        // get current Rigidbody component
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // add force x/z axis
+        rigidbody.AddForce(x, 0, z);
+    }
 }
