1cc20b98b3d01e85ddeed2d467080fa3f334b2d6
gouf <innocent.zero@gmail.com>
2019-01-06 20:02:03 +0900

    カメラ追従にオフセットを追加 (TPS 風)

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour {
 
     public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+    }
 
     private void Update()
     {
-        GetComponent<Transform>().position = target.position;
+        GetComponent<Transform>().position = target.position + offset;
     }
 }
