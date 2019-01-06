8d7ca52ee180a569222113e07113245da0823d27
gouf <innocent.zero@gmail.com>
2019-01-06 22:58:12 +0900

    GameController で Item の数をカウントする

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+    }
+}
