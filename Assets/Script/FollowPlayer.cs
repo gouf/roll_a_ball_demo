f53e8dd543dd50185e25720e5c734925a9651e63
gouf <innocent.zero@gmail.com>
2019-01-06 19:46:22 +0900

    カメラをプレイヤーに追従させる (FPS 風視点)

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform target;
+
+    private void Update()
+    {
+        GetComponent<Transform>().position = target.position;
+    }
 }
