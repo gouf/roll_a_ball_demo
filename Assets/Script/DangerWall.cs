0521d4762468369898cbb3f410071c21d5523af5
gouf <innocent.zero@gmail.com>
2019-01-09 21:43:30 +0900

    ぶつかったらゲームリスタートする壁を配置

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Reflection;
+
+public class DangerWall : MonoBehaviour {
+    public GameObject WinnerLabelObject;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (WinnerLabelObject.activeSelf.Equals(true))
+        {
+            return;
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
