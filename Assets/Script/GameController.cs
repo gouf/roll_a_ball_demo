4cf3ab97cde29e6f26219b510d8c2f0086af2c4e
gouf <innocent.zero@gmail.com>
2019-01-09 20:55:15 +0900

    ゲームクリア時のラベル表示を追加
    "YOU WIN" と表示させてプレイヤーにゲームが完了したことを知らせる

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
     public UnityEngine.UI.Text ScoreLabel;
+    public GameObject winnerLabelObject;
+
     public void Update()
     {
         int count = GameObject.FindGameObjectsWithTag("Item").Length;
         ScoreLabel.text = count.ToString();
+
+        if (count.Equals(0))
+        {
+            winnerLabelObject.SetActive(true);
+        }
     }
 }
